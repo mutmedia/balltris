43f498b89aa4a543cb90392ad9c4a245cc3956ed
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2017-05-02 15:15:40 -0300

    Optimized gaussian blur a bit'

--- game/shaders/gaussianblur2.lua
+++ game/shaders/gaussianblur2.lua
@@ -4,6 +4,8 @@ function GaussianBlurShader(sigma)
   local invSigma2 = sigma > 0 and 1/(sigma*sigma) or 1
   local norm  = 0
 
+  local fragshader = {}
+
   local defines = [[
     #ifdef GL_ES 
     #ifdef GL_FRAGMENT_PRECISION_HIGH
@@ -14,28 +16,29 @@ function GaussianBlurShader(sigma)
     #endif
   ]]
 
-  local fragshader = [[
+  table.insert(fragshader, defines)
+
+  
+  table.insert(fragshader, [[
     uniform vec2 offset_direction;
 
     vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
     {
       vec4 c = vec4(0.0);
-      ]]
+      ]])
 
   for i = -support, support do
     local coef = math.exp(-0.5 * i * i * invSigma2)
     norm = norm + coef
-    fragshader = fragshader..string.format('c += vec4(%f) * Texel(texture, texture_coords + vec2(%f) * offset_direction);\n', coef * 1000, i)
+    local newColor = string.format('c += vec4(%f) * Texel(texture, texture_coords + vec2(%f) * offset_direction);\n', coef * 1000, i)
+    table.insert(fragshader, newColor)
   end
 
-  fragshader = fragshader..string.format('return c * vec4(%f) * color;\n}', norm > 0 and 1/(norm*1000) or 1)
-
-
-  fragshader = defines..fragshader
+  table.insert(fragshader, string.format('return c * vec4(%f) * color;\n}', norm > 0 and 1/(norm*1000) or 1))
 
   --print('Pixel Shader Generated:\n'..fragshader)
 
-  return love.graphics.newShader(fragshader)
+  return love.graphics.newShader(table.concat(fragshader))
 end
 
 return GaussianBlurShader
